91c9c19747588e9efc8fad8bfc4c99cdafffa815
Avi Kabir <48606697+AviMKabir@users.noreply.github.com>
2025-05-21 17:23:53 -0400

    Updated TS for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/wish-you-were-here-v4/',
   plugins: [
     vue(),
     vueJsx(),
